3505b03478a78365140f165ba77d6a1788895b40
Konstantin Babushkin <kostya-babushkin@yandex.ru>
2025-02-17 14:18:38 +0100

    Initial commit: Nuxt & Tailwind CSS setup

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center dark">
+    <div class="bg-green-100 dark:bg-slate-600 text-blue-600 text-center w-40 h-20">111111</div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
