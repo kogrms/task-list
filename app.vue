440406a395da9a5d8baf58dde88e2283b26203a8
Konstantin Babushkin <kostya-babushkin@yandex.ru>
2025-02-20 18:03:48 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative flex flex-col justify-center items-center w-screen h-screen overflow-y-auto"
+    class="relative flex flex-col justify-center items-center w-screen h-[100dvh] overflow-y-auto"
     style="background: linear-gradient(303.79deg, #F0C3FC -53.74%, #D7BEFC -4.64%, #BCB1FC 40.96%, #A1AEFC 83.05%, #94BDFC 127.47%, #8EC5FC 170.73%), linear-gradient(0deg, #84FAB0 0%, #85F9BC 21.87%, #88F8D7 42.19%, #8BF6F5 60.94%, #8EDDF5 80.73%, #8FD3F4 100%);"
   >
     <!-- Dark theme background filter -->
