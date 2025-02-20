9f30d34ac5ab454ba8e4556e3e9fe84d0ec5abd0
Konstantin Babushkin <kostya-babushkin@yandex.ru>
2025-02-20 17:12:27 +0100

    Add api integration & loader

--- app.vue
+++ app.vue
@@ -5,13 +5,19 @@
   >
     <!-- Dark theme background filter -->
     <div class="absolute inset-0 bg-black opacity-0 dark:opacity-30 pointer-events-none w-full h-full z-10"></div>
+
     <div class="relative z-20 flex flex-col items-center w-[95vw] max-w-[730px] min-h-[500px] md:min-h-[719px] bg-white dark:bg-grey-20 rounded-3xl px-4 py-6 sm:py-8">
       <ThemeToggle class="absolute top-6 sm:top-8 right-4 sm:right-8" />
+
       <img src="~assets/images/illustration.png" class="w-[180px] max-w-1/3 md:max-w-[180px] dark:opacity-80"/>
+
       <h1 class="text-3xl font-bold text-center py-4 sm:py-12">Today I need to</h1>
+
       <TaskInput class="mt-1 mb-6 sm:mb-10"/>
-      <TaskSection v-if="tasks.length"/>
-      <div v-else class="flex items-center justify-center gap-2 mt-auto flex-wrap">
+
+      <TaskSection v-if="tasks.length && !loading"/>
+
+      <div v-if="!tasks.length && !loading" class="flex items-center justify-center gap-2 mt-auto flex-wrap">
         <svgo-checkmark
           class="w-4 h-4 sm:w-6 sm:h-6 text-grey-8f shrink-0"
           :font-controlled="false"
@@ -19,14 +25,26 @@
         />
         <p class="text-t12 sm:text-t14 text-grey-8f text-center">Congrats, you have no more tasks to do</p>
       </div>
+
+      <div v-if="loading" class="flex flex-col items-center justify-center h-full">
+        <Loader />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { useTaskStore } from '@/stores/taskStore'
-  import { storeToRefs } from 'pinia'
+import { useTaskStore } from '@/stores/taskStore'
+import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
+import Loader from '@/components/Loader.vue'
 
-  const taskStore = useTaskStore()
-  const { tasks } = storeToRefs(taskStore)
+const taskStore = useTaskStore()
+const { tasks } = storeToRefs(taskStore)
+
+const loading = storeToRefs(taskStore).loading
+
+onMounted(() => {
+  taskStore.fetchTasks()
+})
 </script>
---
+++ components/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <p class="text-t12 sm:text-t14 text-grey-8f mt-2">
+    Loading{{ dots }}
+  </p>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const dots = ref('')
+let intervalId
+
+onMounted(() => {
+  intervalId = setInterval(() => {
+    dots.value = dots.value.length < 3 ? dots.value + '.' : '.'
+  }, 150)
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId)
+})
+</script>
